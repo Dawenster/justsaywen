999e18297a263e7996bcc77184f8b39c190ef4dc
Dawenster <dstwen@gmail.com>
2013-10-31 16:32:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JustSayWen::Application.config.secret_key_base = '10344096651a7ddb0aad5a275c5df9489c3259d988d394aec9dad7b2fe06b24f906075baaf7026cf2c44c15cd993f694c77a303f3c6228031f6c7ce5f7dad709'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JustSayWen::Application.config.session_store :cookie_store, key: '_just_say_wen_session'
