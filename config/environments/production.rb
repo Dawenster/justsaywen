b8e62adc4d0de04712ea32ceeaa92517add57478
Dawenster <dstwen@gmail.com>
2013-11-04 20:42:34 -0800

    clobbered precompile; commented out lines in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ JustSayWen::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # config.serve_static_assets = false # As per http://stackoverflow.com/questions/18828795/rails-4-asset-compiling
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false # As per http://stackoverflow.com/questions/18828795/rails-4-asset-compiling
 
   # Generate digests for assets URLs.
   config.assets.digest = true
