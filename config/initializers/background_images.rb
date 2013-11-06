5602a8ca033e90faff11b86365964d85275f6cb8
Dawenster <dstwen@gmail.com>
2013-11-05 20:52:30 -0800

    low resolution images - attempt for faster load times on mobile

--- config/initializers/background_images.rb
+++ config/initializers/background_images.rb
@@ -1,8 +1,8 @@
 ENV['BACKGROUND_IMAGES'] = {
-  "pages" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_bali.jpg", "position" => "bottom left" },
+  "pages" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_bali-sm.jpg", "position" => "bottom left" },
   "blogs" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_new_york_city.jpg", "position" => "bottom left" },
   "biographies" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_about.jpg", "position" => "center" },
-  "works" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_work.jpg", "position" => "left" },
+  "works" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_work.jpg", "position" => "top left" },
   "galleries" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_photography.jpg", "position" => "center" },
   "books" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_bookshelf.jpg", "position" => "bottom" },
   "travels" => { "url" => "https://s3-us-west-2.amazonaws.com/justsaywen/david_wen_about.jpg", "position" => "center" },
