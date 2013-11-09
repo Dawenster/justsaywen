6654830aff4786d8f548f78545abc7432109b8fa
Dawenster <dstwen@gmail.com>
2013-11-08 16:53:12 -0800

    update read more css; use :filter => text in Tumblr API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def index
     @background = eval(ENV['BACKGROUND_IMAGES'])[params[:controller]]
     client = Tumblr::Client.new
-    @posts = client.posts("dawenster.tumblr.com", :limit => 5)["posts"]
+    @posts = client.posts("dawenster.tumblr.com", :limit => 5, :filter => "text")["posts"]
   end
 end
