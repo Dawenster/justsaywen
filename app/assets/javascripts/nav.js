8d2741a15b267dcbc17a009a55b341adb0cf71bb
Dawenster <dstwen@gmail.com>
2013-11-12 15:15:03 -0800

    fix issue where scroll up button doesn't work after clicking menu logo

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -4,12 +4,15 @@ ready = function() {
     $(".nav-menu").removeClass("hidden");
     $(".site-nav-overlay").removeClass("hidden");
     $("body").attr("style", "overflow: hidden;");
+    if ($(".top-of-page")) {
+      $(".top-of-page").addClass("hidden");
+    }
     showNavMenu();
   });
   $(".clickable").click(function() {
     hideNavMenu();
     $("body").attr("style", "");
-  })
+  });
   $(".menu-logo").on('dragstart', function(e) {
     e.preventDefault();
   });
@@ -36,6 +39,9 @@ var hideOptions = {
     $(".nav-menu").addClass("hidden");
     $(".site-nav-overlay").addClass("hidden");
     $(".menu-logo-dup").addClass("hidden");
+    if ($(".top-of-page")) {
+      $(".top-of-page").removeClass("hidden");
+    }
   }
 }
 
