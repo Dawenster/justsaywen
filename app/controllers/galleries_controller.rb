c6eb994d757e33878512f19ed3ffe9b52a68aefc
Dawenster <dstwen@gmail.com>
2013-11-13 15:56:20 -0800

    galleries#show nav buttons and responsive layout

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -5,4 +5,16 @@ class GalleriesController < ApplicationController
     FlickRaw.shared_secret = ENV['FLICKR_SECRET']
     @sets = flickr.photosets.getList(:user_id => "33229628@N04")
   end
+
+  def show
+    FlickRaw.api_key = ENV['FLICKR_KEY']
+    FlickRaw.shared_secret = ENV['FLICKR_SECRET']
+    sets = flickr.photosets.getList(:user_id => "33229628@N04")
+    sets.each_with_index do |set, i|
+      if set["id"] == params[:flickr_set_id]
+        @next_set = sets[i + 1]
+        @next_set = sets[0] if i == sets.size - 1
+      end
+    end
+  end
 end
