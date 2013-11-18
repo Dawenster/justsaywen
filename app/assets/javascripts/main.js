570bd58da7a194e8d9ab08e13528993a9a259dfa
Dawenster <dstwen@gmail.com>
2013-11-18 13:07:02 -0800

    work section; refactored next article js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,3 +2,9 @@ Galleria.configure({
   debug: false // debug is now off for deployment
 });
 Galleria.loadTheme('/assets/galleria.classic.min.js');
+
+$(document).ready(function() {
+  $(".next-article").click(function() {
+    window.location = $(".next-article").attr("data-next-article-url");
+  });
+});
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -7,13 +7,13 @@ class WorksController < ApplicationController
     client = Tumblr::Client.new
     @work = client.posts("justsaywen.tumblr.com", :tag => params[:slug])["posts"][0]
     @work["first_image"] = find_first_image(@work["body"])
-    @next_work = client.posts("justsaywen.tumblr.com", :filter => "text", :tag => next_work_section(params[:slug]))["posts"].last
+    @next_work = client.posts("justsaywen.tumblr.com", :filter => "text", :tag => next_work(params[:slug]))["posts"].last
   end
 
   private
 
   def next_work(current_work)
-    works = ["time-auction", "to-tablets", "flyshortcut", "sola-caritas", "college-pro", "iya", "axiom-zen", "legalreach", "kpmg", "secor", "rl-solutions"]
+    works = ["time-auction", "to-tablets", "fly-shortcut", "sola-caritas", "college-pro-painters", "iya", "axiom-zen", "legal-reach", "kpmg", "secor", "rl-solutions"]
     current_index = works.index(current_work)
     if current_index == works.size - 1
       return works[0]
