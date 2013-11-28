c0f3e021fb3426cc1d23d731cf7731ccdd46b88f
Dawenster <dstwen@gmail.com>
2013-11-28 10:06:55 -0800

    fix to include imgur gif in find_first_image method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,13 @@ class ApplicationController < ActionController::Base
   helper_method :find_first_image
 
   def find_first_image(str)
-    regexes = [/http:\/\/media.tumblr.com.*.jpg\b/, /http:\/\/media.tumblr.com.*.png\b/, /http:\/\/media.tumblr.com.*.jpeg\b/, /http:\/\/media.tumblr.com.*.gif\b/]
+    regexes = [ 
+      /http:\/\/media.tumblr.com.*.jpg\b/,
+      /http:\/\/media.tumblr.com.*.png\b/,
+      /http:\/\/media.tumblr.com.*.jpeg\b/,
+      /http:\/\/media.tumblr.com.*.gif\b/,
+      /http:\/\/i.imgur.com.*.gif\b/
+    ]
     regexes.each do |regex|
       if str.match regex
         return str.match(regex)[0]
