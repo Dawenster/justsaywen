0e47c0f326f3f5975ed1246e2d3303c840395fe7
Dawenster <dstwen@gmail.com>
2013-11-30 23:55:48 -0800

    further fix to chinese book title issue

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
   before_save :create_slug
 
   def create_slug
-    self.slug = self.to_slug if self.title.parameterize
+    self.slug = self.to_slug unless self.title.parameterize.blank?
   end
 
   def to_slug
