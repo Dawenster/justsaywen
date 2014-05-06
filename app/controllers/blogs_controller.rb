d4a0925c308d480fcf0b9bceeedd57b0ff7a047c
David Wen <dstwen@gmail.com>
2014-05-06 09:03:50 -0400

    fixes to old posts in blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,15 +5,19 @@ class BlogsController < ApplicationController
     client = Tumblr::Client.new
     new_posts = client.posts("dawenster.tumblr.com", :filter => "text", :tag => "new", :offset => page * 20)
     @posts = new_posts["posts"]
-    post_count = @posts.size
-    if post_count < 20 && post_count != 0
-      old_posts = client.posts("dawenster.tumblr.com", :filter => "text", :tag => "old", :limit => 20 - post_count)["posts"]
+    new_post_count = @posts.size
+
+    if new_post_count > 0 && new_post_count < 20 # A page with some new and some old
+      old_posts = client.posts("dawenster.tumblr.com", :filter => "text", :tag => "old", :limit => 20 - new_post_count)["posts"]
       @posts += old_posts
-    elsif post_count == 0
+
+    elsif new_post_count == 0 # A page purely with old
+
       total_posts = new_posts["total_posts"]
-      already_shown = 20 - (total_posts % 20)
-      old_posts = client.posts("dawenster.tumblr.com", :filter => "text", :tag => "old", :offset => already_shown)["posts"]
+      old_posts_shown = (page * 20) - total_posts
+      old_posts = client.posts("dawenster.tumblr.com", :filter => "text", :tag => "old", :offset => old_posts_shown)["posts"]
       @posts += old_posts
+
     end
     @hide_last = true if @posts.last["slug"] == "numero-uno"
   end
