0bb18ad54846713ad567b227d62204c4014d9074
David Wen <dstwen@gmail.com>
2015-10-13 16:17:06 -0700

    Add latest book reviews to landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
     @background = eval(ENV['BACKGROUND_IMAGES'])[params[:controller]]
     client = Tumblr::Client.new
     @posts = client.posts("dawenster.tumblr.com", :limit => 5, :filter => "text")["posts"]
+    @books = Book.last(5)
   end
 end
